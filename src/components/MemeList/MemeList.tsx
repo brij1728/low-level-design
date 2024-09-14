85e71fb46a99115a3deb11d7ef0bb8a1001335e6
Brijesh Kumar <bk10895@gmail.com>
2024-09-14 09:00:49 +0530

    Refactor MemeList component to fetch and display memes with infinite scrolling

--- src/components/MemeList/MemeList.tsx
+++ src/components/MemeList/MemeList.tsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 interface Meme {
   id: string;
-  name: string;
+  title: string;
   url: string;
 }
 
@@ -14,22 +14,43 @@ export const MemeList: React.FC = () => {
   const [memes, setMemes] = useState<Meme[]>([]);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetchMemes();
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   // Fetch meme data
   const fetchMemes = async () => {
     try {
-      const response = await axios.get('https://api.imgflip.com/get_memes');
-      const memes = response.data.data.memes;
-      setMemes(memes);
+      setLoading(true);
+      const response = await axios.get('https://meme-api.com/gimme/20');
+      const memes = response.data.memes;
+      console.log('Fetched memes:', memes);
       setLoading(false);
+      setMemes((currentMemes) => [...currentMemes, ...memes]);
     } catch (error) {
       console.error('Error fetching memes:', error);
       setLoading(false);
     }
   };
 
-  useEffect(() => {
-    fetchMemes();
-  }, []);
+  
+
+  // Infinite scrolling
+  const handleScroll = () => {
+    // Fetch more memes when user reaches the bottom of the page
+    // innerHeight - Height of the browser window
+    // scrollY - Current scroll position
+    // scrollHeight - Height of the entire page
+    if (
+      window.innerHeight + window.scrollY >= document.body.scrollHeight){
+      console.log('Fetching more memes...');
+       fetchMemes();
+      }
+     
+   
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 p-8">
@@ -41,7 +62,7 @@ export const MemeList: React.FC = () => {
       ) : memes.length > 0 ? (
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {memes.map((meme) => (
-            <MemeCard key={meme.id} id={meme.id} name={meme.name} url={meme.url} />
+            <MemeCard key={meme.id} id={meme.id} name={meme.title} url={meme.url} />
           ))}
         </div>
       ) : (
