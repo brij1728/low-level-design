81b17f17ebed13d78edfcfb788971bf19d899246
Brijesh Kumar <bk10895@gmail.com>
2024-09-18 20:34:57 +0530

    Add Gayle McDowell's resume data

---
+++ src/data/gayleResume.ts
@@ -0,0 +1,51 @@
+const textdata: string[] = [];
+const text_content: string = `
+Gayle L. McDowell
+123 Spruce St, Apt 35, Philadelphia PA 19103    (555) 555-1212    gayle@careercup.com
+
+Employment
+
+Software Engineer, Intern - Apple Computer (Summer 2004)
+    • Reduced time to render the user’s buddy list by 75% by implementing prediction algorithm.
+    • Implemented iChat integration with OS X Spotlight Search by creating tool which extracts metadata from saved chat transcripts and provides metadata to a system-wide search database.
+    • Redesigned chat file format and implemented backwards compatibility for search.
+
+Lead Student Ambassador - Microsoft Corporation (Fall 2003 - Spring 2005)
+    • Promoted to Lead Student Ambassador in Fall 2004; supervised 10 – 15 Student Ambassadors.
+    • Created and taught Computer Science course, CSE 099: Software Design and Development.
+
+Head Teaching Assistant - University of Pennsylvania (Fall 2001 - Spring 2005)
+    • Promoted to Head TA in Fall 2004; led weekly meetings and supervised four other TAs.
+    • Courses: Advanced Java III, Software Engineering, Mathematical Foundations of Comp. Sci. I & II.
+
+Software Design Engineer, Intern - Microsoft Corporation (Summers 2001 - 2003)
+    • Implemented a user interface for the VS open file switcher (ctrl-tab) and extended it to tool windows.
+    • Built app to compute similarity of all methods in a code base; reduced time from O(n^2) to O(n log n).
+    • Created service to provide gradient across VS and VS add-ins. Optimized service via caching.
+    • Created test case generation tool which creates random XML docs from XML Schema.
+
+Education
+
+University of Pennsylvania, Philadelphia, PA (Fall 2000 - May 2005)
+    • M.S.E. in Computer and Information Science, May 2005. GPA: 3.6.
+    • B.S.E. in Computer Science Engineering with Minor in Mathematics, May 2005. In-major GPA: 3.4.
+    • Graduate Coursework: Software Foundations; Computer Architecture; Algorithms; Artificial Intelligence; Comparison of Learning Algorithms; Computational Theory.
+    • Undergraduate Coursework: Operating Systems; Databases; Algorithms; Programming Languages; Comp. Architecture; Engineering Entrepreneurship; Calculus III.
+
+Technical Experience
+
+Projects
+    • Multi-User Drawing Tool (2004). Electronic classroom where multiple users can view and simultaneously draw on a “chalkboard” with each person’s edits synchronized. C++, MFC.
+    • Synchronized Calendar (2003 – 2004). Desktop calendar with globally shared and synchronized calendars, allowing users to schedule meetings with other users. C#, .NET, SQL, XML.
+    • Operating System (2002). UNIX-style OS with scheduler, file system, text editor and calculator. C.
+
+Additional Experience and Awards
+    • Instructor (2003 – 2005): Taught two full-credit Computer Science courses; average ratings of 4.8 out of 5.0.
+    • Third Prize, Senior Design Projects: Awarded 3rd prize for Synchronized Calendar project, out of 100 projects.
+
+Languages and Technologies
+    • C++; C; Java; Objective-C; C#.NET; SQL; JavaScript; XSLT; XML (XSD) Schema.
+    • Visual Studio; Microsoft SQL Server; Eclipse; XCode; Interface Builder.
+`;
+
+textdata.push(text_content);
